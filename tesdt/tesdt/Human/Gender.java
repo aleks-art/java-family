96a8bb302ced06b521741d1b667771a0da99053a
aleks-art <66997352+aleks-art@users.noreply.github.com>
2024-04-22 17:52:59 +0300

    Delete tesdt/tesdt directory

--- tesdt/tesdt/Human/Gender.java
+++
@@ -1,5 +0,0 @@
-package tesdt.Human;
-
-public enum Gender {
-    Male, Female
-}
--- tesdt/tesdt/writer/Writeble.java
+++
@@ -1,9 +0,0 @@
-package tesdt.writer;
-
-
-import tesdt.family_tree.FamilyTree;
-
-public interface Writeble {
-    boolean save (FamilyTree serializable, String filename);
-    Object read(String filename);
-}
